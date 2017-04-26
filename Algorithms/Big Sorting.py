fec3de6c269a3381ef8b8f1f1ffa73cab40650fb
KarolR <karolrodzinski@gmail.com>
2017-04-26 23:25:49 +0200

    Create Big Sorting.py

---
+++ Algorithms/Big Sorting.py
@@ -0,0 +1,9 @@
+n = int(raw_input().strip())
+unsorted = []
+unsorted_i = 0
+for unsorted_i in xrange(n):
+    unsorted_t = str(raw_input().strip())
+    unsorted.append(unsorted_t)
+# your code goes here
+
+print "\n".join(sorted(unsorted, cmp=lambda a, b: cmp(a, b) if len(a)==len(b) else len(a)-len(b)))
