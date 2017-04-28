1ea0bc7e6e746969d55d1704d02abe1b8a485d10
KarolR <karolrodzinski@gmail.com>
2017-04-28 14:51:28 +0200

    Simple, short python solution

--- Algorithms/BigSorting.py
+++ Algorithms/BigSorting.py
@@ -5,5 +5,4 @@ for unsorted_i in xrange(n):
     unsorted_t = str(raw_input().strip())
     unsorted.append(unsorted_t)
 # your code goes here
-
 print "\n".join(sorted(unsorted, cmp=lambda a, b: cmp(a, b) if len(a)==len(b) else len(a)-len(b)))
